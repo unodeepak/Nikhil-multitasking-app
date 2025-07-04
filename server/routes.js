2d1f445f599a8e4aad86cc61037c7f5a589b150a
Deepak Keshri <unodeepak1@gmail.com>
2025-07-04 17:00:02 +0530

    Added All the routes

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const userController = require('./api/user/controller/userController');
+const express = require("express");
+const userController = require("./api/user/controller/userController");
+
 
 const route = express.Router();
 
-route.post('/create', userController.createUser)
-route.get('/getAll', userController.getAll)
-route.get('/getDataByName', userController.getDataByName)
+
+route.post("/create", userController.createUser);
+route.get("/getAll", userController.getAll);
+route.get("/getDataByName", userController.getDataByName);
+route.get("/getDataById", userController.getDataById);
+route.put("/updateUserData", userController.updateUserData);
+route.delete("/deleteUserById", userController.deleteUserById);
+
 
 module.exports = route;
